dd258d0d381bf0465ba4f3c73e1333a5038aa81c
Tomasz Laboj <tomasz.laboj@ocado.com>
2025-04-19 16:58:46 +0100

    add delete mapping

--- src/main/java/com/tomasz_laboj/simplified_todo/controller/ToDoController.java
+++ src/main/java/com/tomasz_laboj/simplified_todo/controller/ToDoController.java
@@ -42,4 +42,9 @@ public class ToDoController {
         }
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteToDoItem(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 }
